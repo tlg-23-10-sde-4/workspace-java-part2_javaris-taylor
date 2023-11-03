afbc73b7cb5d1eba1be898c9b9613d81946f2f13
Javaris Taylor <javaris.taylor@tlgcohort.com>
2023-11-03 15:41:36 -0700

    Lab04.2 Generics (Partial)

--- Lab04.2/src/com/javatunes/product/Order.java
+++ Lab04.2/src/com/javatunes/product/Order.java
@@ -11,22 +11,27 @@ package com.javatunes.product;
 import java.util.Collection;
 
 public class Order {
-  private String id;
-  
-  public Order(String id) {
-    this.id = id;
-  }
-  
-  /**
-   * TODO:
-   * get the items from the cart and iterate over them, print each item's product code
-   * get cart total and print
-   */
-  public void processCart(Object reference_to_cart) {
-    
-  }
-  
-  public String getId() {
-    return id;
-  }
+    private String id;
+
+    public Order(String id) {
+        this.id = id;
+    }
+
+    /**
+     * TODO:
+     * get the items from the cart and iterate over them, print each item's product code
+     * get cart total and print
+     */
+    public void processCart(ShoppingCart<? extends Product> cart) {
+        System.out.println("Order: " + getId() + " contains the following.");
+        for(Product item : cart.allItems()) {
+
+            System.out.println(item);
+        }
+        System.out.println("Order Total: " + cart.total());
+    }
+
+    public String getId() {
+        return id;
+    }
 }
