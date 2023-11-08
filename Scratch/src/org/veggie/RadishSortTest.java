1fef560eb2a39567e9d474a758bee617179dec36
Javaris Taylor <javaris.taylor@tlgcohort.com>
2023-11-08 10:35:37 -0800

    RadishSortTest anonymous Comparators (Complete)

--- Scratch/src/org/veggie/RadishSortTest.java
+++ Scratch/src/org/veggie/RadishSortTest.java
@@ -1,6 +1,7 @@
 package org.veggie;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -13,25 +14,44 @@ class RadishSortTest {
         radishes.add(new Radish("black",    1.0,      0,   3));
         radishes.add(new Radish("red",      2.5,    4.2,   0));
         radishes.add(new Radish("pink",     0.7,    3.2,   2));
-
         dump(radishes);
         System.out.println();
+        // Natural Order
         System.out.println("Natural Order");
         radishes.sort(null); // passing null means do by natural order
         dump(radishes);
-
         System.out.println();
+        // Color Order
         System.out.println("color, using Comparator");
-        radishes.sort(new RadishColorComparator());
+        //radishes.sort(new RadishColorComparator());
+        radishes.sort(new Comparator<Radish>(){ // sort by color, via anonymous class
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return r1.getColor().compareTo(r2.getColor());
+            }
+        });
         dump(radishes);
-
         System.out.println();
+        // Sprout Order
         System.out.println("sprouts, using RadishSproutsComparator");
         radishes.sort(new RadishSproutsComparator());
         dump(radishes);
+        System.out.println();
+        // TailLength Order
+        System.out.println("sort by TailLength, via anonymous class");
+        radishes.sort(new Comparator<Radish>() {
+
+            @Override
+            public int compare(Radish r1, Radish r2)  {  // anonymous class hijacks the compare method and inputs its own arguments- allows a "new" interface in a way
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+
+        });
+        dump(radishes);
+        System.out.println();
 
     }
-
+// dump method
     private static void dump(List<Radish> radishes) {
         for(Radish radish : radishes) {
             System.out.println(radish); // toString automatically called
