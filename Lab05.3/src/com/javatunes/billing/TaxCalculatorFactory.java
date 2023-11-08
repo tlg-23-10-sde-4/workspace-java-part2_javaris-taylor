ae3f6a55ea7027290bff3cf2e8eb6e54364f2f24
Javaris Taylor <javaris.taylor@tlgcohort.com>
2023-11-08 07:58:37 -0800

    Lab05.3 TaxCalculatorFactory Singletons (Complete)

--- Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
+++ Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
@@ -4,33 +4,39 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class TaxCalculatorFactory {
-    TreeMap<Location, TaxCalculator> locationCache = new TreeMap<>();
-
+    private static final Map<Location,TaxCalculator> locationCache = new TreeMap<>();
     private TaxCalculatorFactory() {
 
     }
 
     /*
      * Consider implementinga  "cache" of TaxCalculator objects
-     *
      * If I have not created the object, then i'll create it here with new.
-     *
      * if I already have the object, fetch it from the cache and return it, do not create a new one.
      */
     public static TaxCalculator getTaxCalculator(Location location)  {
-        TaxCalculator calc = null;
 
-        switch(location) {
-            case ONLINE:
-                calc = new OnlineTax();
-                break;
-            case USA:
-                calc = new USATax();
-                break;
-            case EUROPE:
-                calc = new EuropeTax();
+        TaxCalculator calc = null;
+        if (!locationCache.containsKey(location)) {
+            switch(location) {
+                case ONLINE:
+                    calc = new OnlineTax();
+                    locationCache.put(location, calc);
+                    break;
+                case USA:
+                    calc = new USATax();
+                    locationCache.put(location, calc);
+                    break;
+                case EUROPE:
+                    calc = new EuropeTax();
+                    locationCache.put(location, calc);
+            }
+
         }
+        calc = locationCache.get(location);
         return calc;
+
+
     }
 
 }
